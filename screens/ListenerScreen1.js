1e765bf3f90de086e27351f12bc4fd09ba7214fc
moura1001 <gmouraneto01@gmail.com>
2021-11-25 09:34:47 -0300

    commit com finalização da primeira parte do exercício (listener 1)

--- screens/ListenerScreen1.js
+++ screens/ListenerScreen1.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+
+import { listener } from '../utils/listeners';
 
 export default function ListenerScreen1() {
   const [updateCount, setUpdateCount] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = listener(() => {
+      setUpdateCount(currentCount => currentCount + 1);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
